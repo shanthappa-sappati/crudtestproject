e7c749245f00fd36ad4bfb2eb87ab5465405501a
shanthappa-sappati <shanthappa.sappati@gmail.com>
2025-11-15 14:33:28 +0530

    Changes from githubDesktop

--- crudtestproject/src/main/java/com/shan/crudtestproject/controller/HomeController.java
+++ crudtestproject/src/main/java/com/shan/crudtestproject/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 @Controller
-class HomeController {
+class HomeController {//
 
 	  String index(Model model) {
 	        model.addAttribute("now", LocalDateTime.now());
